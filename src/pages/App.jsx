636a82fb3ec6a4627c8ee15ec00df55cc341041b
wilberson-roberto <wilberson.roberto.01@gmail.com>
2025-06-24 18:06:41 -0300

    adicionado estilos globais

---
+++ src/pages/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div>
+            <h1>App do Centro Espírita</h1>
+            <p>Bem-vindo(a) ao nosso aplicativo!</p>
+        </div>
+    );
+}
+
+export default App;
